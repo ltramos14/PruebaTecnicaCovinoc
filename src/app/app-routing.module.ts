4f9683b95aecebf637693e8ffd2e2b9a3a14f1c6
Laura Tatiana Ramos Villanueva <48187187+ltramos14@users.noreply.github.com>
2022-06-09 13:45:21 -0500

    Primera parte del front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TasksComponent } from './pages/tasks/tasks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/tareas', pathMatch: 'full' },
+  { path: '**', component: TasksComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TasksComponent } from './pages/tasks/tasks.component';
+import { ToolbarComponent } from './pages/shared/toolbar/toolbar.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input'
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    //Angular Material
+    MatToolbarModule,
+    MatIconModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatTableModule,
+
+
+    //FlexLayout
+    FlexLayoutModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
